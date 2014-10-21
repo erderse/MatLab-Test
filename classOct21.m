8355d2e24edf7153b1609f5edf30251d662c4aa1
erderse <erderse@iastate.edu>
2014-10-21 08:34:00 -0500

    Initial Upload

---
+++ classOct21.m
@@ -0,0 +1,20 @@
+grade = input ('What percent is your grade?');
+    if (grade >= 90)
+        a = 1;
+    elseif (grade >= 80)
+        a = 2;
+    elseif (grade >= 70)
+        a = 3;
+    else
+        a = 4;
+    end
+    switch a
+        case 1
+            disp ('You have an A.');
+        case 2
+            disp ('You have a B.');
+        case 3
+            disp ('You have a C.');
+        case 4
+            disp ('You have a D.');
+    end
